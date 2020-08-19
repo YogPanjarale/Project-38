16cdc073b617da000f092857aaa14c8905a7e0f6
YogPanjarale <64301340+YogPanjarale@users.noreply.github.com>
2020-08-19 18:42:23 +0530

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,7 +1,7 @@
 class SpiderMan extends BaseClass {
   constructor(x,y){
   if(height/20 >40){
-    super(x,y,height/20,height/20);
+    super(x,y,height/12,height/12);
   }else if(height/20 <40){
     super(x,y,50,50);
   }
